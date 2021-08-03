095b4f713638d02fac83bf39f5d6724a6e86475e
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2021-08-03 12:07:53 -0500

    Clean up some displays

--- Models/Password.cs
+++ Models/Password.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PasswordGeneratorApp.Models
 {
     public class Password
     {
         public long PasswordId { get; set; }
+        [Display(Name = "Link to Site")]
         public string SiteURL { get; set; }
+        [Display(Name = "Password")]
         public string SitePassword { get; set; }
+        [Display(Name = "Create Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime CreateDate { get; set; }
         private DateTime expireDate { get; set; }
+        [Display(Name = "Expires")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime ExpireDate 
         {
             get => expireDate;
@@ -18,7 +25,9 @@ namespace PasswordGeneratorApp.Models
                 expireDate = date;
             }
         }
+        [Display(Name = "For")]
         public Person Person { get; set; }
+        [Display(Name = "Associated Person")]
         public long PersonId { get; set; }
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace PasswordGeneratorApp.Models
     public class Person
     {
         public long PersonId { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        public string FullName =>
+            FirstName + " " + LastName;
     }
 }
